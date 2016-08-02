6fafd2f77ab4e4296def322d094cab467133ce98
Steven Chuob <schuob@fastly.com>
2016-08-01 20:23:47 -0700

    updated script
    clean up script with drwilco suggestion

--- make_csshrc.sh
+++ make_csshrc.sh
@@ -2,39 +2,13 @@
 ## Converting ~/.ssh/known_hosts.fastly to a working .csshrc config file.
 
 # Array of POPS
-NA_POP=(JFK IAD ATL BOS DEN ORD MIA DFW SJC LAX SEA YYZ)
-SA_POP=(GRU)
-EU_POP=(FRA AMS LCY LHR BMA)
-APAC_POP=(AKL FJR WLG SYD MEL BNE PER ITM NRT SIN HKG)
+caches=$(grep ^cache- ~/.ssh/known_hosts.fastly | cut -d ',' -f1 | sort | uniq | sort -t- -k2,2 -k3,3 -k4n,4n -k5n,5n  | uniq)
+pops=$(echo "$caches" | sed -E 's/cache-([^0-9]*).*$/\1/' | uniq)
 
-echo "NA_POP = JFK IAD ATL BOS DEN ORD MIA DFW SJC LAX SEA YYZ" >> /etc/clusters
-echo "SA_POP = GRU" >> /etc/clusters
-echo "EU_POP = FRA AMS LCY LHR BMA" >> /etc/clusters
-echo "APAC_POP = AKL FJR WLG SYD MEL BNE PER ITM NRT SIN HKG" >> /etc/clusters
-
-for i in ${NA_POP[@]}; do
-	i_lc=`echo "$i" | awk '{print tolower($0)}'`
-	cache_node=$(grep "cache-${i_lc}" ~/.ssh/known_hosts.fastly | cut -d ',' -f2 | cut -d' ' -f1)
-	output="${i} = ${cache_node}"
-	echo $output >> /etc/clusters
-done
-for i in ${SA_POP[@]}; do
-	i_lc=`echo "$i" | awk '{print tolower($0)}'`
-	cache_node=$(grep "cache-${i_lc}" ~/.ssh/known_hosts.fastly | cut -d ',' -f2 | cut -d' ' -f1)
-	output="${i} = ${cache_node}"
-	echo $output >> /etc/clusters
-done
-for i in ${EU_POP[@]}; do
-        i_lc=`echo "$i" | awk '{print tolower($0)}'`
-	cache_node=$(grep "cache-${i_lc}" ~/.ssh/known_hosts.fastly | cut -d ',' -f2 | cut -d' ' -f1)
-	output="${i} = ${cache_node}"
-	echo $output >> /etc/clusters
-done
-for i in ${APAC_POP[@]}; do
-	i_lc=`echo "$i" | awk '{print tolower($0)}'`
-	cache_node=$(grep "cache-${i_lc}" ~/.ssh/known_hosts.fastly | cut -d ',' -f2 | cut -d' ' -f1)
-	output="${i} = ${cache_node}"
-	echo $output >> /etc/clusters
+for pop in $pops; do 
+  hosts=$(echo "$caches" | grep ^cache-$pop | cut -d '.' -f1) 
+  POP=$(echo $pop | tr [:lower:] [:upper:])
+  echo $POP = $hosts >> /etc/clusters
 done
 
 exit 0
